0939a078e15cac84ca98a251c2134e9cc4f13ea3
Liliya <liliyavoloshine@gmail.com>
2021-09-08 12:16:08 +0400

    feat: finish nav and header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-const navBtn = document.querySelector('#navBtn')
+const navBtn = document.querySelector('#navBtn'),
+navList = document.querySelector('#navList')
 
 navBtn.addEventListener('click', toggleMenu)
 
 
 function toggleMenu() {
   navBtn.classList.toggle('opened')
+  navList.classList.toggle('opened')
 }
